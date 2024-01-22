e1603ef00d8c2317a018e79373029eecd101251d
achrafelabdallaoui <achrafelabdllaoui2@gmail.com>
2024-01-22 16:18:19 +0100

    app done

--- weather/src/components/FetchWeather.jsx
+++ weather/src/components/FetchWeather.jsx
@@ -7,21 +7,39 @@ import { Container } from "react-bootstrap";
 function FetchWeather() {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
+  const [day, setDay] = useState(null);
 
-  const handleSearch = async () => {
+  const fetchSearch = async () => {
     try {
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=472fd5a8a4862dffcd7119a94ee5bc4a&units=metric`
-        // `https://api.openweathermap.org/data/2.5/forecast?q=roma&appid=3f64327af1492cb6b843fb2420be96e9&units=metric`
-      );
-      const data = await response.json();
+        );
+        const data = await response.json();
+        
+        setWeatherData(data);
+      } catch (error) {
+        console.error("Errore nel recupero dei dati meteorologici:", error);
+      }
+    };
 
-      setWeatherData(data);
-    } catch (error) {
-      console.error("Errore nel recupero dei dati meteorologici:", error);
-    }
-  };
-
+    const fetchForecast = async () => {
+      try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3f64327af1492cb6b843fb2420be96e9&lang=it&units=metric`);
+        const data = await response.json();
+        setDay(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching forecast data:", error);
+      }
+    };
+    const handleSearch = () => {
+      if (city.length > 3) {
+        fetchSearch();
+        fetchForecast();
+      }
+    };
+    
+    
   return (
     <>
       <Container className="p-5 radius my-5 rounded-5">
@@ -33,7 +51,7 @@ function FetchWeather() {
         />
       </Container>
       <Container className="d-flex justify-content-center " >
-        <RisultatiMeteo city={city} weatherData={weatherData} />
+        <RisultatiMeteo city={city} weatherData={weatherData} day={day}/>
       </Container>
     </>
   );
--- weather/src/components/RisultatiMeteo.jsx
+++ weather/src/components/RisultatiMeteo.jsx
@@ -1,23 +1,25 @@
+
 import { BiWind } from "react-icons/bi"; 
 import { WiHumidity } from "react-icons/wi"; 
 import { FaTemperatureHigh } from "react-icons/fa"; 
 import { FaTemperatureLow } from "react-icons/fa"; 
 
 
-const RisultatiMeteo = ({city, weatherData}) => (
+const RisultatiMeteo = ({city, weatherData, day}) => (
     <>
-      {weatherData && (
+      {weatherData && day &&(
         <>
-        <div className="bg-light bg-opacity-10 rounded-5">
+        <div className="bg-light bg-opacity-10 rounded-5 w-50 p-2 mb-5">
         <div className="text-center">
         <div className="display-3 "> {city}</div>
+        
         <div className="display-1 ">{weatherData.main.temp}°C</div>
         <div className="display-5 ">
             {weatherData.weather[0].description}
             <img src= {`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png `} height={60}/>
         </div>
         </div>
-        <div className=" d-flex my-4 ">
+        <div className=" d-flex my-4 justify-content-around bg-light bg-opacity-25 rounded-5">
             <div className="mx-3">Temp max:<br/> 
             {weatherData.main.temp_max}°C <FaTemperatureHigh /></div>
             <div className="mx-3">Temp min:<br/> 
@@ -26,6 +28,33 @@ const RisultatiMeteo = ({city, weatherData}) => (
             {weatherData.main.humidity}% <WiHumidity /></div>
             <div className="mx-3">Wind:<br/>
              {weatherData.wind.speed}KM/h <BiWind  /></div>
+        </div>
+        <div className="d-flex justify-content-around mb-3">
+        <div className="text-center bg-light bg-opacity-50 rounded-5 w-25 py-3">
+              <p >Today</p>
+              
+              <p>{day.list[0].main.temp}°C <FaTemperatureHigh /></p>
+              <p>{day.list[0].main.humidity} % <WiHumidity /></p>
+              <p>{day.list[0].wind.speed}  <BiWind  /></p>
+             <img src= {`https://openweathermap.org/img/wn/${day.list[0].weather[0].icon}@2x.png`} alt="" height={60}/>
+         </div>
+         <div className="text-center bg-light bg-opacity-50 rounded-5 w-25 py-3">
+              <p >Tomorrow</p>
+              
+              <p>{day.list[8].main.temp}°C <FaTemperatureHigh /></p>
+              <p>{day.list[8].main.humidity} % <WiHumidity /></p>
+              <p>{day.list[8].wind.speed}  <BiWind  /></p>
+             <img src= {`https://openweathermap.org/img/wn/${day.list[8].weather[0].icon}@2x.png`} alt="" height={60}/>
+         </div>
+         <div className="text-center bg-light bg-opacity-50 rounded-5 w-25 py-3">
+              <p >Day After</p>
+              
+              <p>{day.list[16].main.temp}°C <FaTemperatureHigh /></p>
+              <p>{day.list[16].main.humidity} % <WiHumidity /></p>
+              <p>{day.list[16].wind.speed}  <BiWind  /></p>
+             <img src= {`https://openweathermap.org/img/wn/${day.list[16].weather[0].icon}@2x.png`} alt="" height={60}/>
+         </div>
+                
         </div>
         </div>
        
